0cfa9e8e7c4fe44d3930767bbe7aff879f397572
Joao Vitor <jvads2005@gmail.com>
2024-09-23 14:57:48 -0300

    feat: application service registration and mapping profile

---
+++ DeviceService.Application/Common/Mappings/MapperProfile.cs
@@ -0,0 +1,18 @@
+using AutoMapper;
+using DeviceService.Application.DTOs;
+using DeviceService.Application.Features.Device.Commands;
+using DeviceService.Domain.Entities;
+
+namespace DeviceService.Application.Common.Mappings;
+
+public class MapperProfile : Profile
+{
+    public MapperProfile()
+    {
+        CreateMap<Device, DeviceReponseDTO>().ReverseMap();
+        CreateMap<Device, CreateDeviceCommand>();
+        CreateMap<CreateDeviceCommand, Device>()
+            .ForMember(dest => dest.DeviceType, opt => opt.Ignore())
+            .ForMember(dest => dest.DeviceActions, opt => opt.Ignore());
+    }
+}
