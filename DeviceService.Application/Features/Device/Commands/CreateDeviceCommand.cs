e88c89dd7b6189321a406b0bbb5c43a93d4354dd
Joao Vitor <jvads2005@gmail.com>
2024-09-23 14:57:05 -0300

    feat: add create command and create command handler for device entity

---
+++ DeviceService.Application/Features/Device/Commands/CreateDeviceCommand.cs
@@ -0,0 +1,20 @@
+using System.Text.Json.Serialization;
+using MediatR;
+
+namespace DeviceService.Application.Features.Device.Commands;
+
+public class CreateDeviceCommand(string deviceType, string name, bool? isDimmable) : IRequest<Guid>
+{
+    public required string DeviceType { get; set; } = deviceType;
+
+    [JsonIgnore]
+    public Guid UserId { get; set; } = Guid.NewGuid();
+
+    [JsonIgnore]
+    public Guid RoomId { get; set; } = Guid.NewGuid();
+
+    [JsonIgnore]
+    public Guid? GroupId { get; set; } = Guid.Empty;
+    public required string Name { get; set; } = name;
+    public bool? IsDimmable { get; set; } = isDimmable;
+}
---
+++ DeviceService.Application/Features/Device/Handlers/CommandHandlers/CreateDeviceCommandHandler.cs
@@ -0,0 +1,33 @@
+using AutoMapper;
+using DeviceService.Application.Features.Device.Commands;
+using DeviceService.Domain.Interfaces;
+using MediatR;
+using Entity = DeviceService.Domain.Entities;
+
+namespace DeviceService.Application.Features.Device.Handlers.CommandHandlers;
+
+public class CreateDeviceCommandHandler(
+    IDeviceRepository deviceRepository,
+    IDeviceTypeRepository deviceTypeRepository,
+    IMapper mapper
+) : IRequestHandler<CreateDeviceCommand, Guid>
+{
+    private readonly IDeviceRepository _deviceRepository = deviceRepository;
+    private readonly IDeviceTypeRepository _deviceTypeRepository = deviceTypeRepository;
+    private readonly IMapper _mapper = mapper;
+
+    public async Task<Guid> Handle(CreateDeviceCommand request, CancellationToken cancellationToken)
+    {
+        var deviceType =
+            await _deviceTypeRepository.GetDeviceTypeByNameAsync(request.DeviceType)
+            ?? throw new ArgumentException($"Device type '{request.DeviceType}' not found.");
+
+        var device = _mapper.Map<Entity.Device>(request);
+
+        device.DeviceType = deviceType;
+
+        await _deviceRepository.CreateAsync(device);
+
+        return device.Id;
+    }
+}
