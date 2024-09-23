300e9ed4fea09a8679edcb1bde8087c31623c867
Joao Vitor <jvads2005@gmail.com>
2024-09-23 10:50:47 -0300

    feat: generic repository interface with CRUD tasks for entities repositories

---
+++ DeviceService.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,12 @@
+namespace DeviceService.Domain.Interfaces;
+
+public interface IRepository<T>
+    where T : class
+{
+    Task<IEnumerable<T>> GetAllAsync(int page, int pageSize);
+    Task<T> GetByIdAsync(Guid id);
+    Task<T> CreateAsync(T entity);
+    Task<T> UpdateAsync(T entity);
+    Task<T> DeleteAsync(Guid id);
+    Task<int> CountAsync();
+}
