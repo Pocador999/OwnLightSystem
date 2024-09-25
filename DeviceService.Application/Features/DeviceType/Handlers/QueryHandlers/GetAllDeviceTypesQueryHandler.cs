6f8e596d61d40f8f3cd5a0183691d802fb53ff8d
Joao Vitor <jvads2005@gmail.com>
2024-09-25 16:00:24 -0300

    feat: get all device types method

---
+++ DeviceService.Application/Features/DeviceType/Handlers/QueryHandlers/GetAllDeviceTypesQueryHandler.cs
@@ -0,0 +1,32 @@
+using System;
+using AutoMapper;
+using DeviceService.Application.DTOs;
+using DeviceService.Application.Features.DeviceType.Queries;
+using DeviceService.Domain.Interfaces;
+using MediatR;
+
+namespace DeviceService.Application.Features.DeviceType.Handlers.QueryHandlers;
+
+public class GetAllDeviceTypesQueryHandler(
+    IDeviceTypeRepository deviceTypeRepository,
+    IMapper mapper
+) : IRequestHandler<GetAllDeviceTypesQuery, PaginatedResultDTO<DeviceTypeResponseDTO>>
+{
+    private readonly IDeviceTypeRepository _deviceTypeRepository = deviceTypeRepository;
+    private readonly IMapper _mapper = mapper;
+
+    public async Task<PaginatedResultDTO<DeviceTypeResponseDTO>> Handle(
+        GetAllDeviceTypesQuery request,
+        CancellationToken cancellationToken
+    )
+    {
+        var deviceTypes = await _deviceTypeRepository.GetAllAsync(request.Page, request.PageSize);
+        var response = _mapper.Map<IEnumerable<DeviceTypeResponseDTO>>(deviceTypes);
+        return new PaginatedResultDTO<DeviceTypeResponseDTO>(
+            deviceTypes.Count(),
+            request.Page,
+            request.PageSize,
+            response
+        );
+    }
+}
