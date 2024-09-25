ec3c19b020661c929446a50b58981eb0b756ba96
Joao Vitor <jvads2005@gmail.com>
2024-09-25 16:00:55 -0300

    refact: adding device type mappings to mapper profile

--- DeviceService.Application/Common/Mappings/MapperProfile.cs
+++ DeviceService.Application/Common/Mappings/MapperProfile.cs
@@ -1,6 +1,7 @@
 using AutoMapper;
 using DeviceService.Application.DTOs;
 using DeviceService.Application.Features.Device.Commands;
+using DeviceService.Application.Features.DeviceType.Commands;
 using DeviceService.Domain.Entities;
 
 namespace DeviceService.Application.Common.Mappings;
@@ -9,11 +10,18 @@ public class MapperProfile : Profile
 {
     public MapperProfile()
     {
-        CreateMap<Device, DeviceReponseDTO>().ReverseMap();
+        // Mapping for Device Entity
+        CreateMap<Device, DeviceResponseDTO>().ReverseMap();
         CreateMap<Device, CreateDeviceCommand>();
         CreateMap<CreateDeviceCommand, Device>()
             .ForMember(dest => dest.DeviceType, opt => opt.Ignore())
             .ForMember(dest => dest.DeviceActions, opt => opt.Ignore());
         CreateMap<UpdateDeviceCommand, Device>();
+
+        // Mapping for DeviceType Entity
+        CreateMap<DeviceType, DeviceTypeResponseDTO>().ReverseMap();
+        CreateMap<DeviceType, CreateDeviceTypeCommand>();
+        CreateMap<CreateDeviceTypeCommand, DeviceType>()
+            .ForMember(dest => dest.Devices, opt => opt.Ignore());
     }
 }
