82f9dc454c560186b3f827ecfe1139d264a2f536
Joao Vitor <jvads2005@gmail.com>
2024-10-04 09:42:47 -0300

    refactor: Update DeviceResponseDTO to use Guid for DeviceTypeId property

--- DeviceService.Application/DTOs/DeviceResponseDTO.cs
+++ DeviceService.Application/DTOs/DeviceResponseDTO.cs
@@ -1,11 +1,9 @@
-using DeviceService.Domain.Enums;
-
 namespace DeviceService.Application.DTOs;
 
 public class DeviceResponseDTO
 {
     public Guid Id { get; set; }
-    public required string DeviceType { get; set; }
+    public Guid DeviceTypeId { get; set; }
     public Guid UserId { get; set; }
     public Guid RoomId { get; set; }
     public Guid? GroupId { get; set; }
