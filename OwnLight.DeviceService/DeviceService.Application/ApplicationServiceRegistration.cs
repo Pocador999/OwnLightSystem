41504cd8db0e7be7c6e923f85e9a536afa228c3f
Joao Vitor <jvads2005@gmail.com>
2024-10-08 22:01:06 -0300

    feat(device-service): add IDeviceActionService, IDeviceService interfaces and DeviceActionService, DeviceService implementations

--- OwnLight.DeviceService/DeviceService.Application/ApplicationServiceRegistration.cs
+++ OwnLight.DeviceService/DeviceService.Application/ApplicationServiceRegistration.cs
@@ -1,4 +1,6 @@
 using System.Reflection;
+using DeviceService.Application.Common.Services;
+using DeviceService.Application.Common.Services.Interfaces;
 using DeviceService.Application.Common.Validations.Device;
 using DeviceService.Application.Common.Validations.DeviceAction;
 using DeviceService.Application.Common.Validations.DeviceType;
@@ -8,6 +10,7 @@ using DeviceService.Application.Features.DeviceType.Commands;
 using FluentValidation;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
+using Service = DeviceService.Application.Common.Services;
 
 namespace DeviceService.Application;
 
@@ -18,11 +21,17 @@ public static class ApplicationServiceRegistration
         services.AddAutoMapper(Assembly.GetExecutingAssembly());
         services.AddMediatR(Assembly.GetExecutingAssembly());
 
+        services.AddScoped<IDeviceActionService, DeviceActionService>();
+        services.AddScoped<IDeviceService, Service.DeviceService>();
+
+
+        // Validators for Device Entity
         services.AddTransient<IValidator<CreateDeviceCommand>, CreateDeviceValidator>();
         services.AddTransient<IValidator<UpdateDeviceCommand>, UpdateDeviceValidator>();
         services.AddTransient<IValidator<UpdateDeviceRoomCommand>, UpdateDeviceRoomValidator>();
         services.AddTransient<IValidator<UpdateDeviceGroupCommand>, UpdateDeviceGroupValidator>();
 
+        // Validators for DeviceAction Entity
         services.AddTransient<IValidator<ControlAllUserDevicesCommand>, ControlAllUserDevicesValidator>();
         services.AddTransient<IValidator<ControlDeviceCommand>, ControlDeviceValidator>();
         services.AddTransient<IValidator<ControlGroupCommand>, ControlGroupValidator>();
@@ -31,6 +40,7 @@ public static class ApplicationServiceRegistration
         services.AddTransient<IValidator<DimmerizeGroupCommand>, DimmerizeGroupValidator>();
         services.AddTransient<IValidator<DimmerizeRoomCommand>, DimmerizeRoomValidator>();
 
+        // Validators for DeviceType Entity
         services.AddTransient<IValidator<CreateDeviceTypeCommand>, CreateDeviceTypeValidator>();
         services.AddTransient<IValidator<UpdateDeviceTypeCommand>, UpdateDeviceTypeValidator>();
 
